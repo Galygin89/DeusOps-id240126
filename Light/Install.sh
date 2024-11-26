d2f18eb09c564cd2949ca3f339175b5fa14641d9
Galygin <145587323+Galygin89@users.noreply.github.com>
2024-11-26 20:44:46 +0300

    Update Install.sh

--- Light/Install.sh
+++ Light/Install.sh
@@ -7,15 +7,14 @@ cd /tmp
 mkdir /tmp/Vagrant
 echo "		###  Установка Vagrant  Virtualbox  Extension Pack  ####"
 sleep 5
-sudo apt update && sudo apt install -y vagrant virtualbox virtualbox-ext-pack 
+apt update && apt install -y vagrant virtualbox virtualbox-ext-pack 
 echo "		###  Инициализация Vagrant  ###"
 sleep 5
-cd /tmp/Vagrant/ 
-vagrant init
+cd /tmp/Vagrant/ && vagrant init
 echo "		###  Заменяем Vagrantfile из git репозитория  ###"
 sleep 5
-rm -f /tmp/Vagrant/Vagrantfile && cp -r ~/DeusOps-id240126/Light/Vagrantfile /tmp/Vagrant/Vagrantfile
-cp ~/DeusOps-id240126/Light/nginx.sh /tmp/Vagrant/ && cp ~/DeusOps-id240126/Light/2048-game.sh /tmp/Vagrant/ && cp ~/DeusOps-id240126/Light/nodejs_install_v16.sh /tmp/Vagrant/
+rm -f /tmp/Vagrant/Vagrantfile && cp -r /tmp/git/DeusOps-id240126/Light/Vagrantfile /tmp/Vagrant/Vagrantfile
+cp /tmp/git/DeusOps-id240126/Light/nginx.sh /tmp/Vagrant/ && cp /tmp/git/DeusOps-id240126/Light/2048-game.sh /tmp/Vagrant/ && cp /tmp/git/DeusOps-id240126/Light/nodejs_install_v16.sh /tmp/Vagrant/
 # mkdir /tmp/vbox/ && cd /tmp/vbox/ && echo '* 0.0.0.0/0 ::/0' > /tmp/vbox/networks.conf && chmod 644 /tmp/vbox/networks.conf && mkdir /etc/vbox && mv /tmp/vbox/networks.conf /etc/vbox/
 echo "		###  Запуск Vagrant  ###"
 sleep 2

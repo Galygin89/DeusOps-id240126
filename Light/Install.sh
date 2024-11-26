6e4a58b02e0d63ad21c6ef971609e4ff7aa87b61
Galygin <145587323+Galygin89@users.noreply.github.com>
2024-11-26 13:40:26 +0300

    Update Install.sh

--- Light/Install.sh
+++ Light/Install.sh
@@ -2,14 +2,18 @@
 set -e
 cd /tmp
 mkdir /tmp/Vagrant
-echo "		###  Установка Vagrant и Virtualbox и Extension Pack  ####"
+echo "		###  Установка Vagrant  Virtualbox  Extension Pack  ####"
+sleep 5
 sudo apt update && sudo apt install -y vagrant virtualbox virtualbox-ext-pack 
 echo "		###  Инициализация Vagrant  ###"
+sleep 5
 cd /tmp/Vagrant/ 
 vagrant init
 
 echo "		###  Заменяем Vagrantfile из git репозитория  ###"
+sleep 5
 rm -f /tmp/Vagrant/Vagrantfile && cp ~/DeusOps-id240126/Light/Vagrantfile /tmp/Vagrant/Vagrantfile
 
 echo "		###  Запуск Vagrant  ###"
+sleep 2
 vagrant up

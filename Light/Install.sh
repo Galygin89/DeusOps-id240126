ad62290b7a391ffa23dce29ee9a232057b66c007
Galygin <145587323+Galygin89@users.noreply.github.com>
2024-11-26 14:33:52 +0300

    Update Install.sh

--- Light/Install.sh
+++ Light/Install.sh
@@ -13,7 +13,9 @@ vagrant init
 echo "		###  Заменяем Vagrantfile из git репозитория  ###"
 sleep 5
 rm -f /tmp/Vagrant/Vagrantfile && cp -r ~/DeusOps-id240126/Light/Vagrantfile /tmp/Vagrant/Vagrantfile
-
+cp ~/DeusOps-id240126/Light/nginx.sh /tmp/Vagrant/
+cp ~/DeusOps-id240126/Light/2048-game.sh /tmp/Vagrant/
+cp ~/DeusOps-id240126/Light/nodejs_install_v16.sh /tmp/Vagrant/
 echo "		###  Запуск Vagrant  ###"
 sleep 2
 vagrant up
